495622e849e787440723bf31d51474e5b2ee8a69
Aditya <aipomak11@gmail.com>
2025-05-18 12:05:16 +0530

    Update alarm_clock.py

--- alarm_clock.py
+++ alarm_clock.py
@@ -1,9 +1,9 @@
 import time
 import win32com.client
 speaker=win32com.client.Dispatch("SAPI.SpVoice")
-alarm=input("Enter the alarm time as HH:MM:SS \n")
+alarm=input("Enter the alarm time as HH:MM \n")
 while True:
-    current_time=time.strftime("%H:%M:%S")
+    current_time=time.strftime("%H:%M")
     if current_time==alarm:
         print("It's time get up !! ")
         speaker.speak("Get up!! ")
